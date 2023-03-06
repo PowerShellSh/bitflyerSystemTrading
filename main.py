505e3c0a715be1532de785cd1d7aa2951baef967
Shugoro Kosaka <anodomini60@gmail.com>
2023-03-06 13:20:58 +0900

    イベントモデルを作成

--- main.py
+++ main.py
@@ -67,11 +67,30 @@ if __name__ == "__main__":
     # df.add_sma(7)
     # print(df.value)
 
+    from app.models.events import SignalEvent
+    import datetime
+    import settings
+    import constants
+
+    now = datetime.datetime.utcnow()
+    s = SignalEvent(time=now, product_code=settings.product_code, side=constants.BUY, price=100.0, units=1)
+    s.save()
+
+    signal_events = SignalEvent.get_signal_events_by_count(10)
+    for signal_event in signal_events:
+        print(signal_event.value)
+
+    now = now - datetime.timedelta(minutes=10)
+    signal_events = SignalEvent.get_signal_events_after_time(now)
+    for signal_event in signal_events:
+        print(signal_event.value)
+
+
     # streamThread = Thread(target=stream.stream_ingestion_data)
-    serverThread = Thread(target=start)
+    # serverThread = Thread(target=start)
 
     # streamThread.start()
-    serverThread.start()
+    # serverThread.start()
 
     # streamThread.join()
-    serverThread.join()
+    # serverThread.join()
